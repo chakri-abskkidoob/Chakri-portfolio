ba5e7e1d77b82924f267516430051a37efaf32ed
chakri <chakripasumarthi@gmail.com>
2023-04-01 15:43:02 +0530

    added sidebar

--- chakri-portfolio/src/App.vue
+++ chakri-portfolio/src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <header>
     <div class="left-half">
       <h2><router-link :to="'/'" class="route">Portfolio.</router-link></h2>
@@ -120,4 +120,254 @@ header {
     font-size: 14px;
     padding:5px;
 }
+</style> -->
+
+
+
+
+
+<!-- added sidebar -->
+
+<template>
+  <div>
+    <header>
+      <div class="left-half">
+        <div class="menu-toggle" @click="toggleSidebar">
+          <i class="fas fa-bars"></i>
+        </div>
+        <h2><router-link :to="'/'" class="route">Portfolio.</router-link></h2>
+      </div>
+      <div class="right-half">
+        <div>
+          <router-link :to="'/'" class="route">About</router-link>
+        </div>
+        <div>
+          <router-link :to="'/experience'" class="route">Experience</router-link>
+        </div>
+        <div>
+          <router-link :to="'/education'" class="route">Education</router-link>
+        </div>
+        <div>
+          <router-link :to="'/projects'" class="route">Projects</router-link>
+        </div>
+        <div>
+          <router-link :to="'/certificate'" class="route">Certifications</router-link>
+        </div>
+        <div>
+          <router-link :to="'/interests'" class="route">Interests</router-link>
+        </div>
+        <div>
+          <router-link :to="'/skills'" class="route">Skills</router-link>
+        </div>
+        <div>
+          <router-link :to="'/contact'" class="route">Contact</router-link>
+        </div>
+      </div>
+    </header>
+    <div class="sidebar" :class="{ 'sidebar-active': sidebarOpen }">
+      <div class="close-sidebar" @click="toggleSidebar">
+        <i class="fas fa-times"></i>
+      </div>
+      <div>
+        <router-link :to="'/'" class="route" @click="toggleSidebar">About</router-link>
+      </div>
+      <div>
+        <router-link :to="'/experience'" class="route" @click="toggleSidebar">Experience</router-link>
+      </div>
+      <div>
+        <router-link :to="'/education'" class="route" @click="toggleSidebar">Education</router-link>
+      </div>
+      <div>
+        <router-link :to="'/projects'" class="route" @click="toggleSidebar">Projects</router-link>
+      </div>
+      <div>
+        <router-link :to="'/certificate'" class="route" @click="toggleSidebar">Certifications</router-link>
+      </div>
+      <div>
+        <router-link :to="'/interests'" class="route" @click="toggleSidebar">Interests</router-link>
+      </div>
+      <div>
+        <router-link :to="'/skills'" class="route" @click="toggleSidebar">Skills</router-link>
+      </div>
+      <div>
+        <router-link :to="'/contact'" class="route" @click="toggleSidebar">Contact</router-link>
+      </div>
+    </div>
+    <router-view></router-view>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from './components/footer.vue'
+export default {
+  name: "App",
+  components: {
+    Footer,
+  },
+  data() {
+    return {
+      sidebarOpen: false,
+    };
+  },
+  methods: {
+    toggleSidebar() {
+      this.sidebarOpen = !this.sidebarOpen;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: justify;
+  background-color: #0c0341;
+  color: #2c3e50;
+  width: 100%;
+  height: 100%;
+}
+
+/* width */
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #fff;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #3452ff;
+  border-radius: 10px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #1e2b8f;
+}
+
+/* Add a gradient effect to the scrollbar */
+::-webkit-scrollbar-thumb {
+  background: linear-gradient(to bottom, #3452ff, #1e2b8f);
+}
+
+/* Add a border to the scrollbar */
+::-webkit-scrollbar {
+  border: 1px solid #3452ff;
+}
+
+header {
+  background-color: #03032c;
+  color:#3452ff;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.left-half {
+  text-align: left;
+  width:50%;
+}
+
+.right-half {
+  width:40%;
+  text-align: right;
+  display: flex;
+  justify-content: space-between;
+}
+
+.route{
+    text-decoration: none;
+}
+
+.route :hover{
+    background-color: rgba(221, 241, 70, 0.605);
+    color:white;
+    border-radius:5px;
+    font-size: 14px;
+    padding:5px;
+}
+
+.sidebar {
+  position: fixed;
+  top: 60px;
+  left: 0;
+  bottom: 0;
+  width: 200px;
+  padding: 20px;
+  background-color: #1e2b8f;
+  color: #fff;
+  z-index: 999;
+  transform: translateX(-100%);
+  transition: transform 0.3s ease-in-out;
+}
+
+.sidebar a {
+  color: #fff;
+  text-decoration: none;
+  margin-bottom: 10px;
+  display: block;
+}
+
+.sidebar .close-sidebar {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  color: #fff;
+  font-size: 24px;
+  cursor: pointer;
+}
+
+.sidebar.show-sidebar {
+  transform: translateX(0%);
+}
+
+@media screen and (min-width: 768px) {
+  .sidebar {
+    width: 300px;
+  }
+  
+  .left-half {
+    width: 33%;
+  }
+  
+  .right-half {
+    width: 67%;
+  }
+  
+  .menu-toggle {
+    display: none;
+  }
+}
+
+@media screen and (max-width: 767px) {
+  .left-half {
+    width: 100%;
+  }
+  
+  .right-half {
+    display: none;
+  }
+  
+  .menu-toggle {
+    display: block;
+    position: fixed;
+    top: 20px;
+    right: 20px;
+    color: #fff;
+    font-size: 24px;
+    cursor: pointer;
+  }
+  
+  .sidebar {
+    width: 100%;
+  }
+}
 </style>
+
